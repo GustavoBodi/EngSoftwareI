02107319a782a3a0ad5fc17ec01e796b78ee140e
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-06-24 15:46:52 -0300

    Adiciona classe Cemiterio

--- implementacao/src/cemiterio.py
+++ implementacao/src/cemiterio.py
@@ -1,3 +1,18 @@
+from peca import Peca
+
 class Cemiterio:
-    def __init__(self) -> None:
+    def __init__(self, cor: int) -> None:
+        self.__pecas: list[Peca] = []
+        self.__cor: int = cor
+
+    def removerPecas(self) -> None:
+        raise NotImplementedError()
+
+    def adicionarPeca(self, peca: Peca) -> None:
+        raise NotImplementedError()
+
+    def existePecaCemiterio(self) -> bool:
+        raise NotImplementedError()
+
+    def obterPecas(self) -> list[Peca]:
         raise NotImplementedError()
