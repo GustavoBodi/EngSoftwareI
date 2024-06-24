455d4c574be5f8e066285527531dc2be03c09021
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-06-24 15:57:14 -0300

    Adiciona classe Dado

--- implementacao/src/dado.py
+++ implementacao/src/dado.py
@@ -1,3 +1,21 @@
 class Dado:
     def __init__(self) -> None:
+        self.__valores: list[int] = []
+
+    def zerarDados(self) -> None:
+        raise NotImplementedError()
+
+    def gerarNumero(self) -> int:
+        raise NotImplementedError()
+
+    def duplicarDados(self) -> None:
+        raise NotImplementedError()
+
+    def removerDado(self, valor: int) -> None:
+        raise NotImplementedError()
+
+    def atualizarDadoInterface(self) -> None:
+        raise NotImplementedError()
+
+    def obterValores(self) -> list[int]:
         raise NotImplementedError()
