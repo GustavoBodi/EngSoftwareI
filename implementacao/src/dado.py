6d6c0f6848edd7880f62bc5c7d8a98ba13b8d2ce
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-07-01 06:25:16 -0300

    Coloca as coisas inuteis de "maquina de estado" no dado

--- implementacao/src/dado.py
+++ implementacao/src/dado.py
@@ -5,14 +5,19 @@ class Dado:
     def __init__(self, playerInterface) -> None:
         self.__valores: list[int] = []
         self.__playerInterface = playerInterface
+        self.__inicial = True
 
     def zerarDados(self) -> None:
         self.__valores.clear()
+        self.__inicial = True
         self.atualizarDadoInterface()
 
     def gerarNumero(self) -> int:
         num = choice(range(1, 7, 1))
         self.__valores.append(num)
+
+        if len(self.__valores) == 2:
+            self.__inicial = False
 
         self.atualizarDadoInterface()
         return num
@@ -28,6 +33,8 @@ class Dado:
 
     def removerDado(self, valor: int) -> None:
         self.__valores.remove(valor)
+        if len(self.__valores) == 0:
+            self.__inicial = True
         self.atualizarDadoInterface()
 
     def atualizarDadoInterface(self) -> None:
